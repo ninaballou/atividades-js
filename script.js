9712283bd0114c58eac96aa833320f8cbef87d0a
AtenaDev <haynamed@gmail.com>
2024-09-09 12:17:56 -0300

    exercício de função

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+function media(n1,n2) {
+    var nota1 = n1;
+    var nota2 = n2;
+    var media = (nota1 + nota2)/2;
+    /* console.log(media); */
+
+    return media;
+}
+
+var resulta1 = media(6,7)
+var resulta2 = media(8,7)
+
+console.log(resulta1 + " E " + resulta2);
